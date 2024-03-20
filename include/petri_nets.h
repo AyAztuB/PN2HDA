350d28fbe59f6d390521fbcb03606efaf8d58a1f
AyAztuB <ayaztub@gmail.com>
2024-03-20 21:55:41 +0100

    feat(petri_nets): adding struct petri_net & pn_transition with there creation and destruction functions

--- include/petri_nets.h
+++ include/petri_nets.h
@@ -1,18 +1,25 @@
 #ifndef PETRI_NETS_H
 #define PETRI_NETS_H
 
+#include <stddef.h>
+
 #include "vector.h"
 
-struct transition {
+struct pn_transition {
     char* label; // string
-    Vector(int) previous; // vector<int> where int represent the index of the previous places
-    Vector(int) next; // vector<int> like above but for next places
+    Vector(size_t) preset; // vector<size_t> where int represent the index of the input places
+    Vector(size_t) postset; // vector<size_t> like above but for output places
 };
 
 struct petri_net {
-    Vector(struct transition*) transitions; // vector<struct transition*>
-    Vector(int) marking; // vector<int> where each int represent the number of ressources at the given place
+    Vector(struct pn_transition*) transitions; // vector<struct pn_transition*>
+    Vector(size_t) marking; // vector<size_t> where each int represent the number of ressources at the given place
     // ie: place i has marking[i] ressources
 };
+
+struct pn_transition* pn_transition_new(const char* label);
+void pn_transition_destroy(struct pn_transition* t);
+struct petri_net* petri_net_new(void);
+void petri_net_destroy(struct petri_net* pn);
 
 #endif // PETRI_NETS_H
---
+++ src/petri_nets/petri_nets.c
@@ -0,0 +1,71 @@
+#include "petri_nets.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+struct pn_transition* pn_transition_new(const char* label) {
+    struct pn_transition* t = malloc(sizeof(*t));
+    if (!t) return NULL;
+    t->label = strdup(label);
+    if (!t->label) {
+        free(t);
+        return NULL;
+    }
+    t->preset = vector_new(sizeof(size_t), 10);
+    if (!t->preset) {
+        free(t->label);
+        free(t);
+        return NULL;
+    }
+    t->postset = vector_new(sizeof(size_t), 10);
+    if (!t->postset) {
+        vector_destroy(t->preset);
+        free(t->label);
+        free(t);
+        return NULL;
+    }
+    return t;
+}
+
+void pn_transition_destroy(struct pn_transition* t) {
+    if (!t) return;
+    if (t->label)
+        free(t->label);
+    if (t->preset)
+        vector_destroy(t->preset);
+    if (t->postset)
+        vector_destroy(t->postset);
+    free(t);
+}
+
+struct petri_net* petri_net_new(void) {
+    struct petri_net* pn = malloc(sizeof(*pn));
+    if (!pn) return NULL;
+    pn->marking = vector_new(sizeof(size_t), 0);
+    if (!pn->marking) {
+        free(pn);
+        return NULL;
+    }
+    pn->transitions = vector_new(sizeof(struct pn_transition*), 0);
+    if (!pn->transitions) {
+        vector_destroy(pn->marking);
+        free(pn);
+        return NULL;
+    }
+    return pn;
+}
+
+static void free_pn_transition(void* t, __attribute__((unused))void* unused) {
+    pn_transition_destroy(t);
+}
+
+void petri_net_destroy(struct petri_net* pn) {
+    if (!pn) return;
+    if (pn->marking)
+        vector_destroy(pn->marking);
+    if (pn->transitions) {
+        vector_forall(pn->transitions, free_pn_transition, NULL);
+        vector_destroy(pn->transitions);
+    }
+    free(pn);
+}
